2f5f123f949b10d144cf461779239c3fb22b5a39
PauloFrancoo <43294609+PauloFrancoo@users.noreply.github.com>
2022-08-01 20:51:19 -0300

    Add files via upload

---
+++ Contador/contador/js/index.js
@@ -0,0 +1,49 @@
+var currentNumberWrapper = document.getElementById("currentNumber");
+
+var currentNumber = 0
+
+var soma = document.getElementsByName("adicionar")[0];
+soma.addEventListener("click", increment);
+
+function increment(){
+
+    if (currentNumber >= 10){
+        document.getElementsByName("subtrair").disabled = true;
+    }else{
+        currentNumber = currentNumber + 1;
+    }
+
+    if (currentNumber >= 0) {
+        var cor = document.getElementsByTagName("span")[0]
+        cor.style.color="green"
+    }else{
+        var cor = document.getElementsByTagName("span")[0]
+        cor.style.color="red"
+    }
+
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+
+var sub = document.getElementsByName("subtrair")[0];
+sub.addEventListener("click", decrement);
+
+    function decrement(){
+
+    
+    if (currentNumber <= -10){
+        document.getElementsByName("subtrair").disabled = true;
+    }else{
+        currentNumber = currentNumber - 1;
+    }
+
+    if (currentNumber < 0) {
+        var cor = document.getElementsByTagName("span")[0];
+        cor.style.color="red";
+    }else{
+        var cor = document.getElementsByTagName("span")[0];
+        cor.style.color="green";
+    }
+    
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+//01/08/2022
